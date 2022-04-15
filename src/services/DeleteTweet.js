bbf07698f138eb8680b55bc6b8cc7b65a12c610d
Sree Grandhe <sgrandhe@utexas.edu>
2022-04-15 08:21:56 -0500

    Migrated Delete Tweet

--- src/services/DeleteTweet.js
+++ src/services/DeleteTweet.js
@@ -1,39 +1,35 @@
-import firebase from "../firebase/init";
+import Three0 from '../three0';
 
-const db = firebase.firestore();
+const db = Three0.DB.orbitdb;
 
 export const deleteTweet = (tweetID) => {
-  db.collection("tweets")
-    .doc(tweetID)
-    .delete()
-    .then(() => console.log("Deleted Tweet"))
-    .catch((e) => console.log(e));
-
-  db.collection("likes")
-    .where("tweetID", "==", tweetID)
-    .get()
-    .then((tweetsSnapShot) => {
-      const batch = firebase.firestore().batch();
-      tweetsSnapShot.forEach((tweetDocRef) => {
-        batch.delete(tweetDocRef.ref);
-      });
-      batch
-        .commit()
-        .then(console.log("Deleted Likes"))
+  db.docs(
+    // TODO TWEETS ADDRESS
+    ).then(tweets => {
+      tweets.del(tweetID)
+        .then(() => console.log("Deleted Tweet"))
         .catch((e) => console.log(e));
-    });
+    }).catch((e) => console.log(e));
 
-  db.collection("saves")
-    .where("tweetID", "==", tweetID)
-    .get()
-    .then((tweetsSnapShot) => {
-      const batch = firebase.firestore().batch();
-      tweetsSnapShot.forEach((tweetDocRef) => {
-        batch.delete(tweetDocRef.ref);
-      });
-      batch
-        .commit()
-        .then(console.log("Deleted Saves"))
-        .catch((e) => console.log(e));
-    });
+  db.docs(
+    // TODO LIKES ADDRESS
+  ).then(likes => {
+    let promises = [];
+    likes.query(doc => doc.tweetID === tweetID)
+      .forEach(doc => promises.push(likes.del(doc._id))
+    );
+
+    Promise.all(promises).then(() => console.log("Deleted Likes")).catch((e) => console.log(e));
+  }).catch((e) => console.log(e));
+
+  db.docs(
+    // SAVES LIKES ADDRESS
+  ).then(likes => {
+    let promises = [];
+    likes.query(doc => doc.tweetID === tweetID)
+      .forEach(doc => promises.push(likes.del(doc._id))
+    );
+
+    Promise.all(promises).then(() => console.log("Deleted Saves")).catch((e) => console.log(e));
+  }).catch((e) => console.log(e));
 };
